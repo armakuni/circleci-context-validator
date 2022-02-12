0d06f2c81de621b3c0c94492e6265880f4199729
Tom Oram <tom.oram@armakuni.com>
2022-02-12 22:52:34 +0000

    refactor: move test file helpers to own module

---
+++ test/helpers/test-files.ts
@@ -0,0 +1,14 @@
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import * as os from 'node:os'
+import * as yaml from 'js-yaml'
+
+export const TEMP_DIR = fs.mkdtempSync(path.join(os.tmpdir(), 'circleci-context-validator-tests'))
+
+export function tempFilePath(filename: string): string {
+  return path.join(TEMP_DIR, filename)
+}
+
+export function createTempYamlFile(filename: string, content: any): void {
+  fs.writeFileSync(tempFilePath(filename), yaml.dump(content))
+}
