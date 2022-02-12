7503a6afd22c080acbfd46d41b66e37977be49a5
Tom Oram <tom.oram@armakuni.com>
2022-02-12 22:49:06 +0000

    chore: add --context-definitions

--- src/lib/config-validator.ts
+++ src/lib/config-validator.ts
@@ -1,36 +1,25 @@
 import Ajv, {ErrorObject} from 'ajv'
-import {readFile} from 'node:fs/promises'
-import {join} from 'node:path'
 import * as schema from '../config/context_validator_schema.json'
-import {load} from 'js-yaml'
 import betterAjvErrors from 'better-ajv-errors'
+import {Config} from '../config/config'
 
-export default async function isValid(): Promise<boolean> {
+export default function isValid(jsonProjectConfigContents: Config | unknown): jsonProjectConfigContents is Config {
   // Schema validator to include all errors, not fail at the first.
   const ajv =  new Ajv({allErrors: true})
 
   // Load in users context validator config file parse into data JS data structure
-  try {
-    const yamlProjectConfigPath = join(__dirname, '../../example/context_validator.yml')
-    const yamlProjectConfigContents = await readFile(yamlProjectConfigPath, 'utf8')
-    const jsonProjectConfigContents = load(yamlProjectConfigContents, {json: true})
+  console.log(JSON.stringify(jsonProjectConfigContents, null, '\t'))
 
-    console.log(JSON.stringify(jsonProjectConfigContents, null, '\t'))
+  // compile schema for validation
+  const validate = ajv.compile(schema)
 
-    // compile schema for validation
-    const validate = ajv.compile(schema)
+  const valid = validate(jsonProjectConfigContents)
 
-    const valid = validate(jsonProjectConfigContents)
-    if (!valid) {
-      console.log('Errored :(', validate.errors as ErrorObject[])
+  if (!valid) {
+    console.log('Errored :(', validate.errors as ErrorObject[])
 
-      const output = betterAjvErrors(schema, jsonProjectConfigContents, validate.errors as ErrorObject[])
-      console.log(output)
-
-      return false
-    }
-  } catch (error) {
-    console.log('Unable to process project config', error)
+    const output = betterAjvErrors(schema, jsonProjectConfigContents, validate.errors as ErrorObject[])
+    throw new Error(output)
   }
 
   return true
