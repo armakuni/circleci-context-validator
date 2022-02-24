70d7a2e92cdb0255ac4e502730d6d2a3114258a4
Tom Oram <tom.oram@armakuni.com>
2022-02-24 22:03:45 +0000

    test: Add tests for jsonSchemaCalidator

---
+++ test/validator/json-schema-validator.test.ts
@@ -0,0 +1,27 @@
+import {validateWithJsonSchema, ValidatorError} from '../../src/validator'
+import {JSONSchemaType} from 'ajv'
+import {expect} from 'chai'
+
+describe('json-schema-validator', () => {
+  describe('validateWithJsonSchema', () => {
+    interface Message { message: string }
+
+    const schema: JSONSchemaType<Message> = {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: {type: 'string'},
+      },
+    }
+
+    const validate = validateWithJsonSchema(schema)
+
+    it('returns the input when valid', () => {
+      expect(validate({message: 'hello'})).to.eql({message: 'hello'})
+    })
+
+    it('throws when the input in invalid', () => {
+      expect(() => validate({})).to.throw(ValidatorError, /must have required property 'message'/)
+    })
+  })
+})
