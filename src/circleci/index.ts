851d2f480b2c152374cea6417ce053a16946e402
Tom Oram <tom.oram@armakuni.com>
2022-02-25 18:12:51 +0000

    refactor: Pass fetcher into API functions

--- src/circleci/index.ts
+++ src/circleci/index.ts
@@ -1,16 +1,15 @@
 import {ContextItem, getContexts} from './get-contexts'
-import {createFetcher} from './v2-api'
+import {APIFetcher} from './v2-api'
 import {EnvironmentVariable, getContextEnvironmentVariables} from './get-context-environment-variables'
 
 export * from './types'
+export * from './v2-api'
 export {GetContextsResponse, ContextItem} from './get-contexts'
 
-export async function fetchContexts(ownerId: string, personalAccessToken: string): Promise<ContextItem[]> {
-  const fetcher = createFetcher(personalAccessToken)
+export async function fetchContexts(ownerId: string, fetcher: APIFetcher): Promise<ContextItem[]> {
   return (await getContexts(ownerId)(fetcher)).items
 }
 
-export async function fetchContextEnvironmentVariables(contextId: string, personalAccessToken: string): Promise<EnvironmentVariable[]> {
-  const fetcher = createFetcher(personalAccessToken)
+export async function fetchContextEnvironmentVariables(contextId: string, fetcher: APIFetcher): Promise<EnvironmentVariable[]> {
   return (await getContextEnvironmentVariables(contextId)(fetcher)).items
 }
