cb3a8cb04c8c71417ac9c7d4b92b4247801a9efc
Tom Oram <tom.oram@armakuni.com>
2022-03-04 09:27:08 +0000

    feat: Output error when expected context is missing

--- src/circleci/index.ts
+++ src/circleci/index.ts
@@ -1,5 +1,6 @@
-import {ContextItem, createRequest} from './get-contexts'
+import {ContextItem} from './get-contexts'
 import {APIRequest, mapRequest} from './v2-api'
+import * as GetContexts from './get-contexts'
 import * as GetContextEnvironmentVariables from './get-context-environment-variables'
 import {EnvironmentVariable} from './get-context-environment-variables'
 
@@ -8,7 +9,7 @@ export * from './v2-api'
 export {GetContextsResponse, ContextItem} from './get-contexts'
 
 export const getContexts = (ownerId: string): APIRequest<ContextItem[]> =>
-  mapRequest(response => response.items, createRequest(ownerId))
+  mapRequest(response => response.items, GetContexts.createRequest(ownerId))
 
 export const getContextEnvironmentVariables = (contextId: string): APIRequest<EnvironmentVariable[]> =>
   mapRequest(response => response.items, GetContextEnvironmentVariables.createRequest(contextId))
