72b0fa9b0588b176055ee05b54c86a556cff0a33
Tom Oram <tom.oram@armakuni.com>
2022-03-18 07:24:08 +0000

    refactor: Tidy up APIRequest types

--- src/circleci/request.ts
+++ src/circleci/request.ts
@@ -2,39 +2,37 @@ export type APIFetcher = (path: string) => Promise<any>
 
 type APIRequestFunc<Response> = (fetcher: APIFetcher) => Promise<Response>
 
-export interface APIRequest<Response> {
-  (fetcher: APIFetcher): Promise<Response>
+export interface APIRequest<Response> extends APIRequestFunc<Response> {
   map<NewResponse>(f: (x: Response) => NewResponse): APIRequest<NewResponse>
   flatMap<NewResponse>(f: (a: Response) => APIRequest<NewResponse>): APIRequest<NewResponse>
 }
 
-export function newAPIRequest<Response>(action: APIRequestFunc<Response>): APIRequest<Response> {
-  const result = action
-
-  return Object.assign(result, {
-    flatMap<NewResponse>(f: (a: Response) => APIRequest<NewResponse>): APIRequest<NewResponse> {
-      return newAPIRequest(flatMapRequest(f, result))
-    },
-    map<NewResponse>(f: (x: Response) => NewResponse): APIRequest<NewResponse> {
-      return newAPIRequest(mapRequest(f, result))
-    },
-  }) as APIRequest<Response>
+export namespace APIRequest {
+  export const create: <Response>(action: APIRequestFunc<Response>) => APIRequest<Response> =
+    <Response>(action: APIRequestFunc<Response>) => Object.assign(action, {
+      flatMap<NewResponse>(f: (a: Response) => APIRequest<NewResponse>): APIRequest<NewResponse> {
+        return create(flatMap(f, action))
+      },
+      map<NewResponse>(f: (x: Response) => NewResponse): APIRequest<NewResponse> {
+        return create(map(f, action))
+      },
+    })
 }
-
-const mapRequest: <A, B>(f: (x: A) => B, request: APIRequestFunc<A>) => APIRequestFunc<B> =
-  (f, request) =>
-    fetcher => request(fetcher).then(x => f(x))
-
-const flatMapRequest: <A, B>(f: (a: A) => APIRequestFunc<B>, request: APIRequestFunc<A>) => APIRequestFunc<B> =
-  (f, request) =>
-    async (fetcher: APIFetcher) =>
-      f(await request(fetcher))(fetcher)
 
 export const sequenceRequest: <T>(results: APIRequest<T>[]) => APIRequest<T[]> =
   results =>
-    newAPIRequest(async (fetcher: APIFetcher) =>
+    APIRequest.create(async (fetcher: APIFetcher) =>
       Promise.all(results.map(result => result(fetcher))))
 
 export const constantResponseRequest: <Response>(_: Response) => APIRequest<Response> =
   response =>
-    newAPIRequest(async (_fetcher: APIFetcher) => response)
+    APIRequest.create(async (_fetcher: APIFetcher) => response)
+
+const map: <A, B>(f: (x: A) => B, request: APIRequestFunc<A>) => APIRequestFunc<B> =
+  (f, request) =>
+    fetcher => request(fetcher).then(x => f(x))
+
+const flatMap: <A, B>(f: (a: A) => APIRequestFunc<B>, request: APIRequestFunc<A>) => APIRequestFunc<B> =
+  (f, request) =>
+    async (fetcher: APIFetcher) =>
+      f(await request(fetcher))(fetcher)
